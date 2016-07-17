11307a18f9de7c7734dbb0804a80a1f831f092fe
davembush <davembush@dmbcllc.com>
2016-07-17 16:01:55 -0400

    Step 3

--- katas/roman-to-arabic/romanToArabic.js
+++ katas/roman-to-arabic/romanToArabic.js
@@ -1,4 +1,35 @@
 function romanToArabic(romanNumber){
+    var minusOneTable = {
+        IV: 4,
+        IX: 9,
+        XL: 40,
+        XC: 90,
+        CD: 400,
+        CM: 900
+    };
+    var baseTable = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    // This block is probably inefficient, but it is
+    // easy to reason about.
+    for(var prop in minusOneTable){
+        if(!minusOneTable.hasOwnProperty(prop)){
+           continue;
+        }
+        var rx = new RegExp(prop,'g');
+        if((romanNumber.match(rx) || []).length > 1){
+            throw 'Poorly formed Roman number!';
+        }
+
+    }
+
     switch(romanNumber){
         case 'I':
             return 1;
--- tests/roman-to-arabic/romanToArabic.spec.js
+++ tests/roman-to-arabic/romanToArabic.spec.js
@@ -1,20 +1,38 @@
 
 describe('tests/roman-to-arabic/RomanToArabic.spec.js',function(){
     var returnValue;
+    var minusOneTable = {
+        IV: 4,
+        IX: 9,
+        XL: 40,
+        XC: 90,
+        CD: 400,
+        CM: 900
+    };
     describe('When I is passed in',function(){
         beforeEach(function(){
-            returnValue = romanToArabic("I");
+            returnValue = romanToArabic('I');
         });
         it('should return 1',function(){
             expect(returnValue).toBe(1);
         });
-    })
+    });
     describe('When IV is passed in', function(){
         beforeEach(function(){
-            returnValue = romanToArabic("IV");
+            returnValue = romanToArabic('IV');
         });
         it('should return 4',function(){
             expect(returnValue).toBe(4);
         });
-    })
+    });
+    for(var prop in minusOneTable){
+        if(!minusOneTable.hasOwnProperty(prop)){
+            continue;
+        }
+        describe('When ' + prop + prop + ' is added',(function(propCopy){
+            it('should throw an exception',function(){
+                expect(function(){romanToArabic(propCopy+propCopy)}).toThrow();
+            });
+        }).bind(this,prop));
+    }
 });
